c08e67e690ec889fadb88767550e8d559a478197
. <.>
2022-12-23 21:54:51 -0300

    criação de testes modelo de novas áreas de conteúdo, como apresentação e nossos vídeos

---
+++ main.js
@@ -0,0 +1,6 @@
+alert('Site em desenvolvimento. A página que você está encontra-se em fase de testes. Dúvidas e sugestões consulte "@arthur_yugii" no instagram.')
+
+
+function openMenu() {
+  alert('[ERRO] Função em desenvolvimento!')
+}
